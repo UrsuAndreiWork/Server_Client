264d4d1d492441329e96338fe501c94e1fb922eb
Roberto-Tilvar <roberto.tilvar@gmail.com>
2024-12-02 22:47:29 +0200

    feat: create localhost

--- index.ts
+++ index.ts
@@ -1,5 +1,14 @@
-import react from "react"
+import express from 'express';
 
-export const main =()=>{
-    return 0
-}
+const app = express();
+const PORT = 3000;
+
+// Serve a simple response for the root URL
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to the Localhost Webpage!</h1>');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
